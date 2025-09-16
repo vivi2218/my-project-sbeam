3ff3011ea982c573bb91ad8b378b78550e093f87
Edmond <2073026640@qq.com>
2025-09-16 19:07:21 +0800

    navigation

---
+++ sbeam_web/sbeam/src/components/share/navigaton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- 顶部导航 -->
+    <header>
+      <span class="brand">
+        <!-- <img src=" class="" /> -->
+        <div class="logo">Sbeam</div>
+      </span>
+      <nav>
+        <a href="#" class="btn-cute">首页</a>
+        <a href="#" class="btn-cute">商城</a>
+        <a href="#" class="btn-cute">购物车</a>
+        <a href="#" class="btn-cute">我的订单</a>
+        <a href="#" class="btn-cute">社区</a>
+        <a href="#" class="btn-cute">个人中心</a>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style src="../../assets/Share/Navigation.css"></style>
