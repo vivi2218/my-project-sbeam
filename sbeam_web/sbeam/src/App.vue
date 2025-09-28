16ba32ffcd80e8a36fead678ef3c8aa22196dc06
Edmond <2073026640@qq.com>
2025-09-28 17:53:38 +0800

    cc

--- sbeam_web/sbeam/src/App.vue
+++ sbeam_web/sbeam/src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-
-import navigaton from './components/share/navigaton.vue';
-import PageEssay from './components/PageEssay.vue';
-import Myorder from './views/User/Myorder.vue';
+import navigaton from './components/share/navigaton.vue'
+import PageEssay from './components/PageEssay.vue'
+import Myorder from './views/User/Myorder.vue'
 </script>
 
 <template>
-
   <navigaton class="navi" />
   <!-- <page-essay/> -->
   <Myorder class="Myorder" />
-
 </template>
 
 <style>
@@ -21,19 +18,13 @@ import Myorder from './views/User/Myorder.vue';
   align-items: center;
   /* 横向居中 */
   min-height: 100vh;
-<<<<<<< HEAD
-
-=======
->>>>>>> 9c681b8e700af6476a35b51a64560713ed1ca80f
 }
 
 .Myorder {
   position: absolute;
-<<<<<<< HEAD
-  top:50px;
-=======
+
+  top: 50px;
   top: 60px;
->>>>>>> 9c681b8e700af6476a35b51a64560713ed1ca80f
 }
 
 .navi {
