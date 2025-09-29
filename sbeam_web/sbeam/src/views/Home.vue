68d944afb31d976110a5d7d782c65f7441566e13
Gomidasu <amia0827mzk@gmail.com>
2025-09-29 10:38:47 +0900

    sub3

---
+++ sbeam_web/sbeam/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1 class="page-title">欢迎来到Sbeam社区</h1>
+    <div class="welcome-message">
+      <p>这里是Sbeam的社区平台，您可以：</p>
+      <ul class="feature-list">
+        <li>浏览和搜索游戏</li>
+        <li>按类型筛选游戏</li>
+        <li>管理您的游戏订单</li>
+        <li>查看订单状态和历史</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-message {
+  background-color: #1a1d23;
+  padding: 20px;
+  border-radius: 4px;
+}
+
+.feature-list {
+  margin-top: 15px;
+  margin-left: 20px;
+}
+
+.feature-list li {
+  margin: 8px 0;
+}
+</style>
