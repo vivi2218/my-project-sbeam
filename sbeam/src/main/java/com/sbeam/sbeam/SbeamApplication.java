8359a103cacf72e186999e198be78b3643c98d56
Gomidasu <amia0827mzk@gmail.com>
2025-09-30 10:15:14 +0900

    框架

---
+++ sbeam/src/main/java/com/sbeam/sbeam/SbeamApplication.java
@@ -0,0 +1,13 @@
+package com.sbeam.sbeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbeamApplication.class, args);
+	}
+
+}
