4e297b4040190851ef80bbed50eaff2a5adac728
Gomidasu <amia0827mzk@gmail.com>
2025-10-16 19:05:31 +0900

    10.16

--- sbeam/src/main/java/com/sbeam/sbeam/controller/CommentController.java
+++ sbeam/src/main/java/com/sbeam/sbeam/controller/CommentController.java
@@ -3,6 +3,20 @@ package com.sbeam.sbeam.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sbeam.sbeam.entity.Comment;
+import com.sbeam.sbeam.service.ICommentService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 /**
  * <p>
  *  前端控制器
@@ -14,5 +28,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comment")
 public class CommentController {
+    @Autowired
+    private ICommentService commentService;
 
+    @GetMapping("/getall")
+    public List<Comment> getAllComments() {
+        return commentService.getAllComments();
+    }
+
+    @PutMapping("")
+    public String addComment(@RequestBody Comment comment) {
+        commentService.save(comment);
+        return "Comment added successfully";
+    }
+    
 }
--- sbeam/src/main/java/com/sbeam/sbeam/controller/UserController.java
+++ sbeam/src/main/java/com/sbeam/sbeam/controller/UserController.java
@@ -9,6 +9,7 @@ import com.sbeam.sbeam.service.IUserService;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
